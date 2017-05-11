6de15ac9f7f9afc3fa944864aa5fc5003f5ed218
Joseph Mearman <joseph@mearman.co.uk>
2017-05-11 19:16:21 +0100

    removes commented lines

--- setup_lancache.sh
+++ setup_lancache.sh
@@ -1,6 +1,5 @@
 
 #!/bin/bash
-# sudo mkdir lancache
 sudo mkdir -p lancache/data/logs
 sudo mkdir -p lancache/data/cache
 sudo mkdir -p lancache/data/info
@@ -34,20 +33,16 @@ sleep 1m && sudo service docker stop &
 
 echo
 echo "=== Creating steamcache-dns"
-#sudo docker run --name steamcache-dns --restart=always -d -p 53:53/udp -e STEAMCACHE_IP=192.168.86.5 steamcache/steamcache-dns:latest
 sudo docker run --name steamcache-dns --restart=always -p 53:53/udp -e STEAMCACHE_IP="$(hostname -I | cut -d' ' -f1)" -e USE_GENERIC_CACHE=true -e LANCACHE_IP="$(hostname -I | cut -d' ' -f1)" steamcache/steamcache-dns:latest
 
 sudo service docker start
-#sudo docker run --name lancache --restart=always -p 192.168.86.5:80:80 steamcache/generic:latest
 
 echo
 echo "=== Creating background delayed docker service stop"
 sleep 1m && sudo service docker stop &
 echo
 echo "=== Starting lancache"
-#sudo docker run --name lancache --restart=always -v /mnt/m03/lancache:/data/cache -p 192.168.86.5:80:80 steamcache/generic:latest
 sudo docker run --name lancache --restart=always -v "$(pwd)/lancache/data/cache":/data/cache -v "$(pwd)/lancache/data/logs":/data/logs -v "$(pwd)/lancache/data/info":/data/info -p "$(hostname -I | cut -d' ' -f1)":80:80 steamcache/generic:latest
-#sudo docker run --name lancache --restart=always -v /mnt/m03/lancache:/data -p 192.168.86.5:80:80 steamcache/generic:latest
 
 sudo service docker start
 

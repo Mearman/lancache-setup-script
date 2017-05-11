cc129f04054878b09b3e85228c1564f7a5aa2a7d
Joseph Mearman <joseph@mearman.co.uk>
2017-05-11 19:12:57 +0100

    genericised outpu

--- setup_lancache.sh
+++ setup_lancache.sh
@@ -1,6 +1,12 @@
 
 #!/bin/bash
-sudo chmod -R 777 /mnt/m03/lancache/
+# sudo mkdir lancache
+sudo mkdir -p lancache/data/logs
+sudo mkdir -p lancache/data/cache
+sudo mkdir -p lancache/data/info
+
+sudo chmod -R 777 lancache/
+
 echo
 echo "=== Making sure Docker service is running"
 sudo service docker start
@@ -29,7 +35,7 @@ sleep 1m && sudo service docker stop &
 echo
 echo "=== Creating steamcache-dns"
 #sudo docker run --name steamcache-dns --restart=always -d -p 53:53/udp -e STEAMCACHE_IP=192.168.86.5 steamcache/steamcache-dns:latest
-sudo docker run --name steamcache-dns --restart=always -p 53:53/udp -e STEAMCACHE_IP=192.168.86.5 -e USE_GENERIC_CACHE=true -e LANCACHE_IP=192.168.86.5 steamcache/steamcache-dns:latest
+sudo docker run --name steamcache-dns --restart=always -p 53:53/udp -e STEAMCACHE_IP="$(hostname -I | cut -d' ' -f1)" -e USE_GENERIC_CACHE=true -e LANCACHE_IP="$(hostname -I | cut -d' ' -f1)" steamcache/steamcache-dns:latest
 
 sudo service docker start
 #sudo docker run --name lancache --restart=always -p 192.168.86.5:80:80 steamcache/generic:latest
@@ -40,12 +46,12 @@ sleep 1m && sudo service docker stop &
 echo
 echo "=== Starting lancache"
 #sudo docker run --name lancache --restart=always -v /mnt/m03/lancache:/data/cache -p 192.168.86.5:80:80 steamcache/generic:latest
-sudo docker run --name lancache --restart=always -v /mnt/m03/lancache/cache:/data/cache -v /mnt/m03/lancache/logs:/data/logs -v /mnt/m03/lancache/info:/data/info -p 192.168.86.5:80:80 steamcache/generic:latest
+sudo docker run --name lancache --restart=always -v "$(pwd)/lancache/data/cache":/data/cache -v "$(pwd)/lancache/data/logs":/data/logs -v "$(pwd)/lancache/data/info":/data/info -p "$(hostname -I | cut -d' ' -f1)":80:80 steamcache/generic:latest
 #sudo docker run --name lancache --restart=always -v /mnt/m03/lancache:/data -p 192.168.86.5:80:80 steamcache/generic:latest
 
 sudo service docker start
 
-sudo chmod -R 777 /mnt/m03/lancache/
+sudo chmod -R 777 lancache/
 
 echo
 echo "=== --------------------"

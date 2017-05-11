435721b081f3ab89f9fe99025a51263a3f3c9e14
Joseph Mearman <joseph@mearman.co.uk>
2017-05-12 00:10:21 +0100

    adds game information from steamdb

--- watch_lancache.sh
+++ watch_lancache.sh
@@ -7,5 +7,14 @@ echo "Watching logs..."
 
 tail -F ${STEAMCACHE_LOGS}/access.log | while read LINE; do
   echo
+  GAMEID="$(echo ${LINE} | grep '\/depot' | sed 's/.*\/depot\/\([0-9]*\)\/.*/\1/')"
+  echo "GAME_ID = ${GAMEID}"
+  if ! [ -f ${STEAMCACHE_DEPOTINFO}/${GAMEID} ]; then
+    wget -O ${STEAMCACHE_DEPOTINFO}/${GAMEID} https://steamdb.info/depot/${GAMEID}
+  fi
+  G="$(cat ${STEAMCACHE_DEPOTINFO}/${GAMEID} | grep -o '</span>.*</h1>' | sed -e 's/<[^>]*>//g' | sed -e 's/ Content$//')"
+  echo
+  echo "GAME_ID = ${GAMEID}"
+  echo "GAME_NAME =${G}"
   echo "${LINE}" | GREP_COLOR='01;31' egrep -i --color=always '^.*MISS.*$|$'| GREP_COLOR='01;32' egrep -i --color=always '^.*HIT.*$|$'
 done

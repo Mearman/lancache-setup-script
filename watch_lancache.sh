e23a7d025e1f6230a44e5c7b5166e4b974aae1c4
Joseph Mearman <joseph@mearman.co.uk>
2017-05-12 21:01:26 +0100

    added handling of encoded apostrophes

--- watch_lancache.sh
+++ watch_lancache.sh
@@ -6,6 +6,13 @@ STEAMCACHE_DEPOTINFO=data/info
 echo "Watching logs..."
 
 tail -F ${STEAMCACHE_LOGS}/access.log | while read LINE; do
+echo
+echo "Current cache size:"
+echo "$(sudo du -hs --block-size=1000000 cache  | cut -f -1)MB"
+echo "$(sudo du -hs cache  | cut -f -1)"
+echo
+echo "=================================================="
+tail -F -n5 ${STEAMCACHE_LOGS}/access.log | while read LINE; do
   IP="$(echo ${LINE} | grep -o '192.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}')"
   NAME="$(nslookup $IP | sed -n 's/.*arpa.*name = \(.*\)/\1/p')"
 
@@ -26,6 +33,7 @@ tail -F ${STEAMCACHE_LOGS}/access.log | while read LINE; do
       wget -O ${STEAMCACHE_DEPOTINFO}/${GAMEID} https://steamdb.info/depot/${GAMEID}
     fi
     G="$(cat ${STEAMCACHE_DEPOTINFO}/${GAMEID} | grep -o '</span>.*</h1>' | sed -e 's/<[^>]*>//g' | sed -e 's/ Content$//')"
+    G=$(echo $G | sed "s/\&#039;/\'/g")
     echo "Steam download: ${G}"
   fi
   echo "${LINE}" | GREP_COLOR='01;31' egrep -i --color=always '^.*MISS.*$|$'| GREP_COLOR='01;32' egrep -i --color=always '^.*HIT.*$|$'

68af2c11284b4afa08581eadfdfff53957965deb
Joseph Mearman <joseph@mearman.co.uk>
2017-05-12 19:00:54 +0100

    added recognition and announcment of cache type

--- watch_lancache.sh
+++ watch_lancache.sh
@@ -6,15 +6,28 @@ STEAMCACHE_DEPOTINFO=data/info
 echo "Watching logs..."
 
 tail -F ${STEAMCACHE_LOGS}/access.log | while read LINE; do
-  echo
-  GAMEID="$(echo ${LINE} | grep '\/depot' | sed 's/.*\/depot\/\([0-9]*\)\/.*/\1/')"
-  echo "GAME_ID = ${GAMEID}"
-  if ! [ -f ${STEAMCACHE_DEPOTINFO}/${GAMEID} ]; then
-    wget -O ${STEAMCACHE_DEPOTINFO}/${GAMEID} https://steamdb.info/depot/${GAMEID}
+  if [[ $LINE == *"windowsupdate"* ]]; then
+    echo "Windows update:"
   fi
-  G="$(cat ${STEAMCACHE_DEPOTINFO}/${GAMEID} | grep -o '</span>.*</h1>' | sed -e 's/<[^>]*>//g' | sed -e 's/ Content$//')"
-  echo
-  echo "GAME_ID = ${GAMEID}"
-  echo "GAME_NAME =${G}"
+
+  if [ "$LINE" = *"blizzard"* ] || [ "$LINE" == *"blzd"* ] ; then
+    echo "Blizzard download:"
+  fi
+
+  if [[ $LINE == *"steam"* ]]; then
+    echo "Steam download:"
+
+    echo
+    GAMEID="$(echo ${LINE} | grep '\/depot' | sed 's/.*\/depot\/\([0-9]*\)\/.*/\1/')"
+    echo "GAME_ID = ${GAMEID}"
+    if ! [ -f ${STEAMCACHE_DEPOTINFO}/${GAMEID} ]; then
+      wget -O ${STEAMCACHE_DEPOTINFO}/${GAMEID} https://steamdb.info/depot/${GAMEID}
+    fi
+    G="$(cat ${STEAMCACHE_DEPOTINFO}/${GAMEID} | grep -o '</span>.*</h1>' | sed -e 's/<[^>]*>//g' | sed -e 's/ Content$//')"
+    echo
+    echo "GAME_ID = ${GAMEID}"s
+    echo "GAME_NAME =${G}"
+  fi
   echo "${LINE}" | GREP_COLOR='01;31' egrep -i --color=always '^.*MISS.*$|$'| GREP_COLOR='01;32' egrep -i --color=always '^.*HIT.*$|$'
+  echo
 done
